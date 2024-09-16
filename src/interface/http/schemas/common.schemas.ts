54b8c302b8de3d9881267ccf38914f558cbd89b7
David Silva <contatodavidmaia@gmail.com>
2024-09-16 18:59:03 -0300

    fix: update 400 error schema

--- src/interface/http/schemas/common.schemas.ts
+++ src/interface/http/schemas/common.schemas.ts
@@ -5,15 +5,17 @@ export const zodErrorSchema = z.object(
     message: z.string({
       description: 'Zod error message.',
     }),
-    errors: z.object(
-      {
-        fields: z.record(z.array(z.string())),
-        form: z.array(z.string()),
-      },
-      {
-        description: 'Zod error fields and form.',
-      },
-    ),
+    errors: z
+      .object(
+        {
+          fields: z.record(z.array(z.string())),
+          form: z.array(z.string()),
+        },
+        {
+          description: 'Zod error fields and form.',
+        },
+      )
+      .optional(),
   },
   {
     description: 'Zod error response.',
